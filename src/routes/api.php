05799037e3cbed1bde7bb64e70e405c4a4a9ebe1
Kodak4400 <special0747@gmail.com>
2024-01-29 01:26:33 +0900

    wip

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/welcome', function () {
+    \Log::debug('Test Debug Log', ['welcome' => 'start']);
+    \Log::info('Test Info Log', ['welcome' => 'start']);
+    \Log::warning('Test Warning Log', ['welcome' => true]);
+    \Log::error('Test Error Log', ['welcome' => false]);
+    \Log::info('Test Info Log', ['welcome' => 'end']);
+
+    return response()->json([
+        'status' => true,
+        'message' => "Product Created successfully!",
+    ], 200);
 });
